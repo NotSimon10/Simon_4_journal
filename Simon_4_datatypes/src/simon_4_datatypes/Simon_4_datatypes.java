04575d068ef3f7a89f538f691a22522261f1dc32
D22 <NotSimon@ninja5.d22.lan>
2015-10-06 07:21:08 -0500

    This is my program for datatypes.

---
+++ Simon_4_datatypes/src/simon_4_datatypes/Simon_4_datatypes.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simon_4_datatypes;
+public class Simon_4_datatypes {
+    public static void main(String[] args) {
+        int i = 51;
+        float a = 7.11f;
+        double b = 11.0;
+        short c = 100;
+        char d1 = '1';
+        char d2 = 'd';
+        char d3 = 122;
+        String e = "why certainly";
+        boolean f = false;
+        boolean g = true;
+          /*
+        here we will look  at some mathematical operators
+        */
+        // + this adds two numbers and more
+        // - this subtracts two numbers and more
+        // / this divides two numbers and more
+        // % this is modulo.
+        
+        
+//       System.out.println(i + " an integer");
+//       System.out.println(a + " a float");        
+//       System.out.println(b + " a double");
+//       System.out.println(c + " a short");
+//       System.out.println(d1 + " a char");
+//       System.out.println(d2 + " a char");
+//       System.out.println(d3 + " a char");
+//       System.out.println(e + " a string");
+//       System.out.println(f + " a boolean");
+//       for(i = -123;i < 123; i++) {
+//    	   System.out.println(i % 4 == 0);
+//       }
+       // % checks for the remainder
+       // i % 2 checks if a number
+       // is even or odd */
+       //
+//       i = 22;
+//       a = 0;
+//       e = "ninja";
+//        System.out.println(i + a + " an integer" + "plus a float ")
+    	/*adding an int and a string
+    	 * concatenates the int and string as
+    	 * a string
+    	 */
+//        a = 2.5f;
+//        b = 3.2;
+//        System.out.println(a + " a float/ " + "plus a double");
+//        
+//        b = 3.1;
+//        c = 16;   
+          
+       for ( i = 0; i < 500; i++){
+          /*if is a conditional (flow)
+          statement. If a condition is true
+          the following command will be
+          executed
+          */
+          if(i%2==0){
+          System.out.println(i + " is even");
+          } else {
+          System.out.println(i + " is odd");
+          }
+       
+     
+        
+        
+    }
+       
+    }
+    
+    
